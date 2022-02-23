81c010573592d5507040e1199ff37f4937a1067b
Spark265 <vigneshspark24@gmail.com>
2022-02-23 22:20:30 +0530

    My First Commit and Push to Cloud Repo

---
+++ Python/f2.py
@@ -0,0 +1,2 @@
+The GUI and CLI main difference is that the GUI permits users to interact with the system using graphical features like icons, windows, and menus. In contrast, the CLI allows users to interact with the system via commands. To conclude, the GUI is more user-friendly, whereas the CLI is more complex and powerful.
+
